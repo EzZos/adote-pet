cd0418b7d4dc494fecf2dae903c040c4a4b77c8a
João Zumaêta <ezzoscode@gmail.com>
2022-04-14 01:21:44 -0300

    NEW: Model done; Swiper Update; Footer done

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+// SWIPER
+
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
-    freeMode: true,
     keyboard: true,
+    mousewheel: true,
     breakpoints: {
         767: {
             slidesPerView: 2,
@@ -13,4 +15,32 @@ const swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
 });
+
+// MODAL
+
+function activeModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add("show");
+    modal.addEventListener("click", (e) => {
+        if(e.target.id == modalID || e.target.className == "icon-x"){
+            modal.classList.remove("show");
+        }
+    })
+}
+
+const pet = document.querySelectorAll(".pet");
+
+for(const element of pet) {
+    element.addEventListener("click", (event) => {
+        activeModal(event.path[0].id + "-modal");
+    })
+}
